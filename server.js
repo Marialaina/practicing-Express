a2d10059a3e04f445857e4f99b594fc150f7365c
marialaina <pmarialaina@gmail.com>
2021-03-09 19:15:44 -0700

    adding another page using res.send

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const app = express();
 
 //Define a "root" route directly on app
 //app.get is a method (function)
-app.get('/hello', function(req, res){
-    res.send('<h1> Hello Marialaina! </h1>')
+app.get('/', function(req, res){
+    res.send('<h1> Hello Marialaina! </h1>');
 });
+
+app.get(`/home`, function(req, res){
+    res.send(`<h1> Home Page </h1>`);
+})
 
 //Tell the app to listen on port 3000
 //for HTTP requests from clients
