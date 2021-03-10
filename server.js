6693958ba2b55432d767c138c98a65a77d1b2bab
marialaina <pmarialaina@gmail.com>
2021-03-10 08:48:32 -0700

    rendering backend to the screen using a ternary operator. Changing render to redirect.

--- server.js
+++ server.js
@@ -4,17 +4,30 @@ const express = require('express');
 //Creating the express app
 const app = express();
 
+//require the todo "database"
+const todoDb = require(`./data/todo-db`);
+
+console.log(todoDb)
 app.set("view engine", "ejs");
 //Define a "root" route directly on app
 //app.get is a method (function)
 app.get('/', function(req, res){
-    res.send('<h1> Hello Marialaina! </h1>');
+    res.redirect('/home');
 });
 
 app.get(`/home`, function(req, res){
     res.render('home');
 })
 
+//adding a new route for to-do
+
+app.get('/todos', (req, res) => {
+    res.render(`todos/index`, {
+        todos: todoDb.getAll()
+    })
+})
+
+console.log(todoDb)
 //Tell the app to listen on port 3000
 //for HTTP requests from clients
 app.listen(3000, () => {
