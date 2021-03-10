5b788f53eb75f7051d92c12f844cd8af0f631b17
marialaina <pmarialaina@gmail.com>
2021-03-09 18:34:09 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+//loading express
+const express = require('express');
+
+//Creating the express app
+const app = express();
+
+//Define a "root" route directly on app
+//app.get is a method (function)
+app.get('/', function(req, res){
+    res.send('<h1> Hello World! </h1>')
+});
+
+//Tell the app to listen on port 3000
+//for HTTP requests from clients
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
